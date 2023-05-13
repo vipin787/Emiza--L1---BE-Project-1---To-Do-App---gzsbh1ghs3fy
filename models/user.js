b9e8902da7daec44d992b14ba0cf4f92750d31d5
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2023-05-13 20:23:21 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,22 +1,26 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-/*
-Complete your other field here
-
-1. name should be a string and required
-2. password should be a string and required
-3. role should be a string and it can have only 2 values ('user', 'admin') and default should be user
-4. email should be a string and required.
-5. Also Enable timestamps which will store the date of document created and updated.
-*/
-
-var userSchema = mongoose.Schema({
-  //Write your code here.
-},
-
-{ /* ... */ }
-
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    email: {
+      type: String,
+      required: true,
+    }, 
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", userSchema);
-
